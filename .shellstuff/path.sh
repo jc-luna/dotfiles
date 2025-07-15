0afb57257b2758859391df7914e4c0dee3e28b34
JC <jc.lunaa@protonmail.com>
2025-07-15 02:48:45 -0500

    Cargo to Path

--- .shellstuff/path.sh
+++ .shellstuff/path.sh
@@ -1,5 +1,6 @@
 export PATH=/home/luna/develop/personal/cli/bin:$PATH
 export PATH=$PATH:/home/luna/.local/bin
+export PATH=$PATH:/home/luna/.cargo/bin
 export PATH=$PATH:/home/luna/program/lsp
 export PATH=$PATH:/usr/local/texlive/2025/bin/x86_64-linux
 export PATH=$PATH:/home/luna/.spicetify
