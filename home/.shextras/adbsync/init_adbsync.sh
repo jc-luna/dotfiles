d852783a9b846650022a804de867e7551dba6ba8
JC Luna <jc.lunaa@protonmail.com>
2025-09-29 10:48:25 -0500

    Explain adbsync

--- home/.shextras/adbsync/init_adbsync.sh
+++ home/.shextras/adbsync/init_adbsync.sh
@@ -9,8 +9,5 @@ adb push rsync-aarch64 /data/local/tmp/rsync
 echo "Forwarding port $ADBSYNC_PORT on the connected Android device and this computer..."
 adb forward tcp:$ADBSYNC_PORT tcp:$ADBSYNC_PORT
 
-echo "Starting up the rsync server on the connected Android device..."
+echo "Creating config for rsyncd..."
 adb shell "cd /data/local/tmp && echo -e 'address = 127.0.0.1\nport = $ADBSYNC_PORT\n\n[root]\npath = /\nuse chroot = false\nread only = false' > ./rsyncd.conf && echo -e '\nThe rsync daemon config:' && cat ./rsyncd.conf && sleep 1"
-
-#echo "starting server"
-#adb shell "cd /data/local/tmp && ./rsync --daemon --detach --config=./rsyncd.conf"
